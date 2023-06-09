745711a1cd8e022e5390642be3ec668d957685fe
solemannoushad <solemannoushad31@gmail.com>
2023-06-09 15:04:59 +0500

    q1

--- Screens/Home.js
+++ Screens/Home.js
@@ -1,12 +1,29 @@
-import { Text, View, FlatList } from 'react-native'
+import { Text, View, FlatList, Platform } from 'react-native'
 import { styles } from '../Styles/mainStyles'
 import GetDbData from '../Hooks/GetDbData';
 import { useEffect, useState } from 'react';
 import Splash from './Splash'
 import Layout from './Layout';
+import { doc, updateDoc } from "firebase/firestore";
+import { db } from '../config';
+
+
+
 
 
 export default function Home() {
+  useEffect(() => {
+    const addDoc = async()=>{
+
+      const docRef = doc(db, "OS", "0");
+      await updateDoc(docRef, {
+        name: Platform.OS
+      });
+    }
+
+    addDoc();
+
+  } , []);
 
   const [products, setProducts] = useState([]);
   const [splash, setSplash] = useState(true)
@@ -26,7 +43,7 @@ export default function Home() {
           <FlatList
             data={products}
             keyExtractor={(item) => item.id}
-            horizontal={true}
+            horizontal={false}
             showsHorizontalScrollIndicator={false}
             renderItem={({ item }) => (
               <View style={{alignItems: 'center', justifyContent: 'center'}}>
